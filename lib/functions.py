11dda80ab04acf296e505d1b9fa0f39f5f133aaf
111111-glitch <ngigimelissa@gmail.com>
2024-03-03 10:50:54 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,30 @@
-#!/usr/bin/env python3
-
 def greet_programmer():
     pass
+    print(f"Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Naureen")
 
 def add(num1, num2):
     pass
+    return num1 + num2 
+
+add(2, 3)
 
 def halve(number):
     pass
+    return number / 2
+
+halve(10)
